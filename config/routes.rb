72ca94a904b7830d70670fb604cb9afcd3e59a91
Andrew <akshay@kreatio.com>
2016-02-14 20:12:48 +0530

    abt us and read more pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/readmore'
+  get 'home/aboutus'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  #root 'home#index'
+  #root 'home#readmore'
+  root 'home#aboutus'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
